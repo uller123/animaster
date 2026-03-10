108ba9644f09cae83dc4733a26f160cd604cebe2
Yury Shevarkov <shevaryury@gmail.com>
2026-03-10 17:46:08 +0500

    1-3

--- index.js
+++ index.js
@@ -4,20 +4,84 @@ function addListeners() {
     document.getElementById('fadeInPlay')
         .addEventListener('click', function () {
             const block = document.getElementById('fadeInBlock');
-            fadeIn(block, 5000);
+            animaster().fadeIn(block, 5000);
         });
 
     document.getElementById('movePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('moveBlock');
-            move(block, 1000, {x: 100, y: 10});
+            animaster().move(block, 1000, {x: 100, y: 10});
         });
 
     document.getElementById('scalePlay')
         .addEventListener('click', function () {
             const block = document.getElementById('scaleBlock');
-            scale(block, 1000, 1.25);
+            animaster().scale(block, 500, 5);
         });
+    document.getElementById('moveAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('moveAndHideBlock');
+            animaster().moveAndHide(block, 1000, {x: 100, y: 20});
+        });
+    document.getElementById('showAndHidePlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('showAndHideBlock');
+            animaster().showAndHide(block, 1000);
+        });
+    document.getElementById('heartBeatingPlay')
+        .addEventListener('click', function () {
+            const block = document.getElementById('heartBeatingBlock');
+            animaster().heartBeating(block, 500);
+        });
+    
+}
+
+function animaster(){
+    const animaster = {
+        fadeIn: fadeIn,
+        move: move,
+        scale: scale,
+        fadeOut: fadeOut,
+        moveAndHide: moveAndHide,
+        showAndHide: showAndHide,
+        heartBeating: heartBeating
+    };
+    function fadeIn(element, duration) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.remove('hide');
+        element.classList.add('show');
+    }
+
+    function move(element, duration, translation) {
+        element.style.transitionDuration = `${duration}ms`;
+        element.style.transform = getTransform(translation, null);
+    }
+
+    function scale(element, duration, ratio) {
+    element.style.transitionDuration =  `${duration}ms`;
+    element.style.transform = getTransform(null, ratio);
+}
+    function fadeOut(element, duration) {
+        element.style.transitionDuration =  `${duration}ms`;
+        element.classList.remove('show');
+        element.classList.add('hide');
+    }
+    function moveAndHide(element, duration, translation) {
+        move(element, duration * 0.4, translation);
+        setTimeout(() => fadeOut(element, duration * 0.6), duration * 0.4);
+    }
+    function showAndHide(element, duration) {
+        fadeIn(element, duration * 0.4);
+        setTimeout(() => fadeOut(element, duration * 0.6), duration * 0.4);
+    }
+    function heartBeating(element, duration) {
+        setInterval(() => scale(element, duration * 0.5, 1.4), duration);
+        setInterval(() => scale(element, duration * 0.5, 1), duration * 2);
+        
+    }
+
+return animaster;
+
 }
 
 /**
@@ -25,11 +89,7 @@ function addListeners() {
  * @param element — HTMLElement, который надо анимировать
  * @param duration — Продолжительность анимации в миллисекундах
  */
-function fadeIn(element, duration) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.classList.remove('hide');
-    element.classList.add('show');
-}
+
 
 /**
  * Функция, передвигающая элемент
@@ -37,10 +97,6 @@ function fadeIn(element, duration) {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param translation — объект с полями x и y, обозначающими смещение блока
  */
-function move(element, duration, translation) {
-    element.style.transitionDuration = `${duration}ms`;
-    element.style.transform = getTransform(translation, null);
-}
 
 /**
  * Функция, увеличивающая/уменьшающая элемент
@@ -48,10 +104,7 @@ function move(element, duration, translation) {
  * @param duration — Продолжительность анимации в миллисекундах
  * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
  */
-function scale(element, duration, ratio) {
-    element.style.transitionDuration =  `${duration}ms`;
-    element.style.transform = getTransform(null, ratio);
-}
+
 
 function getTransform(translation, ratio) {
     const result = [];
